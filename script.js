ad68cbacfbbd39022bb3460bf63ab4f50cf83d8c
Zachattack221 <zsvd09@gmail.com>
2022-08-23 16:27:13 -0400

    removed highscores HTML page and opted for a change state display of scores, made question list array and added scores state to JS

--- script.js
+++ script.js
@@ -4,28 +4,45 @@ var state = 'begin';
 var beginEl = document.querySelector("#begin");
 var testingEl = document.querySelector("#testing");
 var finishEl = document.querySelector("#finish");
+var scoringEl = document.querySelector("high-scores");
 var questionOptionEl = document.querySelector("#question-option");
 var questionEl = document.querySelector("#testing #question-name");
-var questionOrder = ['First', 'Second', 'Third', 'Fourth', 'Fifth'];
+// made question order an array of objects, with possible answers in an array format for each question
+var questionOrder = [
+    {title:'First', possibleAnswers: [1,2,3,4]}, 
+    {title:'Second', possibleAnswers: [1,2,3,4]},
+    {title:'Third', possibleAnswers: [1,2,3,4]},
+    {title:'Fourth', possibleAnswers: [1,2,3,4]},
+];
+
 var num = 0;
 
-// set up logic to switch display state between begin, testing, and finish sections/ids
+// set up logic to switch display state between begin, testing, finish, and scores
 
 function displayState() {
     if (state === 'begin') {
         beginEl.style.display = 'block';
         testing.style.display = 'none';
         finishEl.style.display = 'none';
+        scoringEl.style.display = 'none';
     }
     if (state === 'testing') {
         beginEl.style.display = 'none';
         testing.style.display = 'block';
         finishEl.style.display = 'none';
+        scoringEl.style.display = 'none';
     }
     if (state === 'finish') {
         beginEl.style.display = 'none';
         testing.style.display = 'none';
         finishEl.style.display = 'block';
+        scoringEl.style.display = 'none';
+    }
+    if (state === 'scores') {
+        beginEl.style.display = 'none';
+        testing.style.display = 'none';
+        finishEl.style.display = 'none';
+        scoringEl.style.display = 'block';
     }
 }
 // set logic to  run displayState() function on init()
@@ -46,13 +63,18 @@ function reset() {
 // score validator - if correct go to next question, else subtract 10
 // function to handle time
 // high score - local storage?
-
+// <!-- TODO: add some sort of form element to store high scores -->
+// <!-- create buttons to navigate back to quiz start and to reset stored scores -->
 
 function displayQuestion() {
     state = 'testing';
     var questionName = questionOrder[Num]
 }
 
+
+startBtn
+
+// function for quiz state needs ha
 // TODO: attach timer to quiz, initialized with start button element, set to subtract remaining time from counter with incorrect answer
 // TODO: trigger timer to end quiz when time = 0
 
